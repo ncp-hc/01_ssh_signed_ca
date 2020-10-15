f34d590fa1a7abf7b28f4cbddbe58c71373342be
JS Park <jsp@hashicorp.com>
2020-10-15 10:20:19 +0900

    Update vault_agent.sh

--- iac/compute-node/templates/vault_agent.sh
+++ iac/compute-node/templates/vault_agent.sh
@@ -8,7 +8,7 @@ sudo ufw allow 22
 sudo ufw --force enable
 sudo timedatectl set-timezone KST
 
-# Vault Install - Host Key Signing 시 사용 목적으로 Host에 Vault를 
+# Vault Install - Host Key Signing 시 사용 목적으로 Host에 Vault를 설치 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install vault
