a5e6ba23ca4dcfc643fbfcee8615e293e8b454dc
JS Park <jsp@hashicorp.com>
2020-10-15 10:14:44 +0900

    Create vault_agent.sh

---
+++ iac/compute-node/templates/vault_agent.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y curl unzip ufw jq sshpass
+
+sudo ufw allow 8200 # vault
+sudo ufw allow 22
+sudo ufw --force enable
+sudo timedatectl set-timezone KST
+
+# Vault Install 
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install vault
